808f91988906204537b6721a9f0d5e5643492586
juhosimola21 <79316712+juhosimola21@users.noreply.github.com>
2023-06-29 22:01:24 +0300

    Follow The Path

---
+++ FollowThePath.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class FollowThePath : MonoBehaviour {
+
+    public Transform[] waypoints;
+
+    [SerializeField]
+    private float moveSpeed = 1f;
+
+    [HideInInspector]
+    public int waypointIndex = 0;
+
+    public bool moveAllowed = false;
+
+	// Use this for initialization
+	private void Start () {
+        transform.position = waypoints[waypointIndex].transform.position;
+	}
+	
+	// Update is called once per frame
+	private void Update () {
+        if (moveAllowed)
+            Move();
+	}
+
+    private void Move()
+    {
+        Debug.Log("waypoints.Length: " + waypoints.Length);
+        Debug.Log("waypointIndex: " + waypointIndex);
+
+        if (waypointIndex < waypoints.Length )
+        {
+            transform.position = Vector2.MoveTowards(transform.position,
+            waypoints[waypointIndex].transform.position,
+            moveSpeed * Time.deltaTime);
+
+            if (transform.position == waypoints[waypointIndex].transform.position)
+            {
+                waypointIndex += 1;
+            }
+        }
+    }
+}
