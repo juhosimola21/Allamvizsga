064fcc9554286904aaff7c16d9dd10d47f4bb9f8
juhosimola21 <79316712+juhosimola21@users.noreply.github.com>
2023-06-29 22:00:54 +0300

    Game Control

---
+++ GameControl.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameControl : MonoBehaviour {
+
+    private static GameObject whoWinsTextShadow, player1MoveText, player2MoveText;
+
+    public static GameObject player1, player2;
+
+    public static int diceSideThrown = 0;
+    public static int player1StartWaypoint = 0;
+    public static int player2StartWaypoint = 0;
+
+    public static bool gameOver = false;
+    public RectTransform WhoWinsText;
+
+    //public static QuizManager quizManager;
+    //[SerializeField] internal QuizManager quiz;
+    // Use this for initialization
+    void Start () {
+
+        //whoWinsTextShadow = GameObject.Find("WhoWinsText");
+        Debug.Log("Who position:" + WhoWinsText.transform.localPosition);
+        player1MoveText = GameObject.Find("Player1MoveText");
+        player2MoveText = GameObject.Find("Player2MoveText");
+
+        player1 = GameObject.Find("Player1");
+        player2 = GameObject.Find("Player2");
+
+        player1.GetComponent<FollowThePath>().moveAllowed = false;
+        player2.GetComponent<FollowThePath>().moveAllowed = false;
+
+        //whoWinsTextShadow.gameObject.SetActive(false);
+        player1MoveText.gameObject.SetActive(true);
+        player2MoveText.gameObject.SetActive(false);
+
+        //quiz = new QuizManager();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player1.GetComponent<FollowThePath>().waypointIndex > 
+            player1StartWaypoint + diceSideThrown)
+        {
+            player1.GetComponent<FollowThePath>().moveAllowed = false;
+            player1MoveText.gameObject.SetActive(false);
+            player2MoveText.gameObject.SetActive(true);
+            player1StartWaypoint = player1.GetComponent<FollowThePath>().waypointIndex - 1;
+        }
+
+        if (player2.GetComponent<FollowThePath>().waypointIndex >
+            player2StartWaypoint + diceSideThrown)
+        {
+            player2.GetComponent<FollowThePath>().moveAllowed = false;
+            player2MoveText.gameObject.SetActive(false);
+            player1MoveText.gameObject.SetActive(true);
+            player2StartWaypoint = player2.GetComponent<FollowThePath>().waypointIndex - 1;
+        }
+
+        if (player1.GetComponent<FollowThePath>().waypointIndex == 
+            player1.GetComponent<FollowThePath>().waypoints.Length)
+        {
+            //whoWinsTextShadow.gameObject.SetActive(true);
+            WhoWinsText.gameObject.SetActive(true);
+            //whoWinsTextShadow.GetComponent<Text>().text = "Player 1 Wins";
+            WhoWinsText.GetComponent<Text>().text = "1. JÁTÉKOS NYERT";
+            gameOver = true;
+        }
+
+        if (player2.GetComponent<FollowThePath>().waypointIndex ==
+            player2.GetComponent<FollowThePath>().waypoints.Length)
+        {
+            //whoWinsTextShadow.gameObject.SetActive(true);
+            WhoWinsText.gameObject.SetActive(true);
+            player1MoveText.gameObject.SetActive(false);
+            player2MoveText.gameObject.SetActive(false);
+            //whoWinsTextShadow.GetComponent<Text>().text = "Player 2 Wins";
+            WhoWinsText.GetComponent<Text>().text = "2.JÁTÉKOS NYERT";
+            gameOver = true;
+        }
+    }
+
+    public void MovePlayer(int playerToMove)
+    {
+        switch (playerToMove) { 
+            case 1:
+                //player1.GetComponent<QuizManager>().generateQuestion();
+                //quiz.generateQuestion();     
+                player1.GetComponent<FollowThePath>().moveAllowed = true;
+                break;
+
+            case 2:
+                player2.GetComponent<FollowThePath>().moveAllowed = true;
+                break;
+        }
+    }
+}
